ed40afbf459db86c9b484fc7ddf8ef5a6624ea37
vidya12 <vidya.bolkaranda@gmail.com>
2016-10-14 15:10:42 +0000

    Complete tenants and users

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -2,16 +2,17 @@ class Tenant < ActiveRecord::Base
 
    acts_as_universal_and_determines_tenant
   has_many :members, dependent: :destroy
-
+  validates_presence_of :name
+  validates_uniqueness_of :name
     def self.create_new_tenant(tenant_params, user_params, coupon_params)
 
-      tenant = Tenant.new(:name => tenant_params[:name])
+      tenant = Tenant.new(tenant_params)
 
       if new_signups_not_permitted?(coupon_params)
 
-        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time" 
+        raise ::Milia::Control::MaxTenantExceeded, "Sorry, new accounts not permitted at this time"
 
-      else 
+      else
         tenant.save    # create the tenant
       end
       return tenant
@@ -43,5 +44,5 @@ class Tenant < ActiveRecord::Base
       #
     end
 
-   
+
 end
